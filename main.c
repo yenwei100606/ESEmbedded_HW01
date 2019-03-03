3b8f19149b806c5fcfa2b61f6ea969f8a016d5f2
yenwei100606 <yenwei100606@gmail.com>
2019-03-03 22:36:44 +0800

    homework01

--- main.c
+++ main.c
@@ -5,19 +5,28 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*cal[])(int,int)=
+{
+	plus,
+	minus,
+	multiply,
+	divided
+};
+char *operation[4]={'+','-','*','/'};
 int main()
 {
-	int a, c;
+	int a, c, i=0;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	for(i;i<4;i++)
+	{
+		if(b==operation[i])
+		
+		{
+			printf("%d %c %d = %d",a,b,c,cal[i](a,c));
+			break;
+		}
+			
+	}
 }
